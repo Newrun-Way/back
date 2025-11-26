c450e0c2c90ae73714febad2c52ddc35a386c3f6
XinKer-Kim <keyhoon.hg@gmail.com>
2025-11-26 14:49:32 +0900

    [fix] org -> original 오타수정

--- app/api/v1/endpoints/async_upload.py
+++ app/api/v1/endpoints/async_upload.py
@@ -36,7 +36,7 @@ async def upload_document(
     # 문서 메타데이터 DB 저장
     metadata = doc_service.create(
         doc_id=doc_id,
-        org_filename=file.filename,
+        original_filename=file.filename,
         user_id=user_id,
         dept_id=dept_id,
         project_id=project_id,
