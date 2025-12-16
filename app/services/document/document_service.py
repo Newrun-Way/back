a39983b9b931eb4b963e752a93ebf26dfd957617
XinKer-Kim <keyhoon.hg@gmail.com>
2025-12-16 09:22:42 +0900

    [feat] DocumentTitlesRequest 정의

--- app/services/document/document_service.py
+++ app/services/document/document_service.py
@@ -3,6 +3,9 @@ from app.core.db import get_connection
 from datetime import datetime
 from typing import List
 
+class DocumentTitlesRequest(BaseModel):
+    doc_ids: List[int]
+
 class DocumentService:
     def __init__(self):
         pass
